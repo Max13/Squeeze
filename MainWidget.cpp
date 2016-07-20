d90a7f9a86cb45de3010b4528f550de68f8a5cb3
Adnan RIHAN <adnan@rihan.fr>
2016-07-20 15:49:44 +0200

    Massively improved search

--- MainWidget.cpp
+++ MainWidget.cpp
@@ -15,6 +15,7 @@ MainWidget::MainWidget(QSettings &settings, QWidget *parent) : QWidget(parent), 
     this->m_fsWidget = new QListWidget(this);
     this->m_hideOkButton = new QPushButton(QStringLiteral("Cacher OK"), this);
     this->m_layout = new QGridLayout(this);
+    this->m_loader = new QProgressBar(this);
     this->m_pathLine = new QLineEdit(this->m_set.value(QStringLiteral("history/dir")).toString(), this);
     this->m_processButton = new QPushButton(QStringLiteral("Confirmer"), this);
     this->m_searchButton = new QPushButton(QStringLiteral("Sélectionner"), this);
@@ -26,10 +27,14 @@ MainWidget::MainWidget(QSettings &settings, QWidget *parent) : QWidget(parent), 
     this->m_layout->addWidget(this->m_pathLine, 0, 0, 1, 2);
     this->m_layout->addWidget(this->m_searchButton, 0, 2);
     this->m_layout->addWidget(this->m_startButton, 1, 0, 1, -1);
+    this->m_layout->addWidget(this->m_loader, 1, 0, 1, -1);
     this->m_layout->addWidget(this->m_fsWidget, 2, 0, 1, -1);
     this->m_layout->addWidget(this->m_hideOkButton, 3, 0, 1, 1);
     this->m_layout->addWidget(this->m_showOkButton, 3, 1, 1, 1);
     this->m_layout->addWidget(this->m_processButton, 3, 2, 1, 1);
+
+    this->m_loader->setRange(0, 0);
+    this->m_loader->setHidden(true);
 
     this->m_pathLine->setMinimumWidth(250);
     this->m_pathLine->setPlaceholderText(QStringLiteral("Chemin"));
@@ -44,40 +49,6 @@ MainWidget::MainWidget(QSettings &settings, QWidget *parent) : QWidget(parent), 
     this->connect(this->m_searchButton, &QPushButton::clicked, this, &MainWidget::setPath);
     this->connect(this->m_showOkButton, &QPushButton::clicked, this, &MainWidget::showOk);
     this->connect(this->m_startButton, &QPushButton::clicked, this, &MainWidget::startCrawling);
-}
-
-void MainWidget::dump(const DirStruct *current, int depth, bool last)
-{
-    QString output;
-    if (depth == -1) {
-        if (!current->entries.isEmpty()) {
-            this->dump(current->entries.first(), depth + 1);
-        }
-        return;
-    }
-
-    if (depth == 1) {
-        output.append(last ? "└" : "├").append("── ");
-    } else if (depth > 1) {
-        output.append("│").append(QString((depth - 1) * 3, ' ')).append(last ? "└" : "├").append("── ");
-    }
-    output.append(current->name);
-
-    qDebug().noquote() << output;
-    this->m_fsWidget->addItem(output);
-    if (output.length() > MAX_LENGTH) {
-        this->m_fsWidget->item(this->m_fsWidget->count() - 1)->setTextColor(QColor(Qt::red));
-    } else if (depth) {
-        this->m_fsWidget->item(this->m_fsWidget->count() - 1)->setHidden(true);
-    }
-
-    if (!current->entries.isEmpty()) {
-        foreach (const DirStruct *item, current->entries) {
-            this->dump(item, depth + 1, item == current->entries.last());
-        }
-    }
-
-    qDebug().resetFormat();
 }
 
 // SLOTS
@@ -105,25 +76,48 @@ void    MainWidget::setPath(void)
 
 void    MainWidget::startCrawling(void)
 {
-    QDir        dir(this->m_pathLine->text());
-    DirStruct   *entries = new DirStruct;
+    this->m_fsWidget->clear();
+    this->m_okItems.clear();
+    this->m_startButton->setHidden(true);
+    this->m_loader->setHidden(false);
 
-    entries->name = QDir::rootPath();
-    entries->entries.append(this->crawl(QFileInfo(this->m_pathLine->text()), true));
+    this->crawl(QFileInfo(this->m_pathLine->text()));
 
-    this->dump(entries);
-    this->m_fsWidget->item(0)->setFlags(Qt::NoItemFlags);
+    this->m_loader->setHidden(true);
+    this->m_startButton->setHidden(false);
+
     this->m_hideOkButton->setEnabled(true);
     this->m_showOkButton->setEnabled(true);
     this->m_processButton->setEnabled(true);
 }
 
-DirStruct   *MainWidget::crawl(const QFileInfo &info, bool isRoot)
+void    MainWidget::crawl(const QFileInfo &info, quint16 depth, bool last)
 {
-    DirStruct       *entry = new DirStruct;
+    bool            isOk(info.fileName().length() <= MAX_LENGTH);
     QFileInfoList   entriesList;
+    QListWidgetItem *rootItem = new QListWidgetItem(this->m_fsWidget);
+    QString         text;
 
-    entry->name = isRoot ? info.canonicalFilePath() : info.fileName();
+    if (depth == 0) {
+        text = info.canonicalFilePath();
+        rootItem->setFlags(Qt::NoItemFlags);
+    } else if (depth == 1) {
+        text.append(last ? "└" : "├").append("── ").append(info.fileName());
+    } else if (depth > 1) {
+        text.append("│").append(QString(depth * 3 + 1, ' ')).append(last ? "└" : "├").append("── ").append(info.fileName());
+    }
+
+    rootItem->setData(MainWidget::FULLPATH, info.canonicalFilePath());
+    rootItem->setData(MainWidget::VISIBILITY, isOk);
+    rootItem->setText(text);
+    if (depth != 0) {
+        if (isOk) {
+            rootItem->setHidden(isOk);
+            this->m_okItems.append(rootItem);
+        } else {
+            rootItem->setTextColor(Qt::red);
+        }
+    }
 
     if (info.isDir()) {
         entriesList = QDir(info.canonicalFilePath()).entryInfoList(
@@ -131,39 +125,23 @@ DirStruct   *MainWidget::crawl(const QFileInfo &info, bool isRoot)
             QDir::Name
         );
 
-        foreach (const QFileInfo &entryInfo, entriesList) {
-            if (entryInfo.isDir()) {
-                entry->entries.append(this->crawl(entryInfo));
-            } else {
-                DirStruct   *file = new DirStruct;
-                file->name = entryInfo.fileName();
-                entry->entries.append(file);
-            }
+        for (QFileInfoList::ConstIterator i(entriesList.cbegin()); i != entriesList.cend(); ++i) {
+            this->crawl(*i, depth + 1, *i == entriesList.last());
         }
     }
-
-    return entry;
 }
 
 void    MainWidget::hideOk(void)
 {
-    for (int i=1,z=this->m_fsWidget->count(); i<z; i++) {
-        QListWidgetItem *item = this->m_fsWidget->item(i);
-
-        if (item->text().length() <= MAX_LENGTH && !item->isHidden()) {
-            item->setHidden(true);
-        }
+    for (QLinkedList<QListWidgetItem*>::Iterator i(this->m_okItems.begin()); i != this->m_okItems.end(); ++i) {
+        (*i)->setHidden(true);
     }
 }
 
 void    MainWidget::showOk(void)
 {
-    for (int i=1,z=this->m_fsWidget->count(); i<z; i++) {
-        QListWidgetItem *item = this->m_fsWidget->item(i);
-
-        if (item->text().length() <= MAX_LENGTH && item->isHidden()) {
-            item->setHidden(false);
-        }
+    for (QLinkedList<QListWidgetItem*>::Iterator i(this->m_okItems.begin()); i != this->m_okItems.end(); ++i) {
+        (*i)->setHidden(false);
     }
 }
 
--- MainWidget.hpp
+++ MainWidget.hpp
@@ -1,7 +1,7 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
-#define MAX_LENGTH  250
+#define MAX_LENGTH  100
 
 #include <QDir>
 #include <QFileInfo>
@@ -10,23 +10,26 @@
 #include <QLinkedList>
 #include <QListWidget>
 #include <QPair>
+#include <QProgressBar>
 #include <QPushButton>
 #include <QSettings>
 #include <QString>
 #include <QWidget>
 
-typedef struct  s_DirStruct {
-        QString                     name;
-        QLinkedList<s_DirStruct*>   entries;
-} DirStruct;
-
 class MainWidget : public QWidget
 {
+    enum    fsRole {
+        FULLPATH    = Qt::UserRole,
+        VISIBILITY  = FULLPATH + 1,
+    };
+
     Q_OBJECT
     private:
         QGridLayout *m_layout;
         QLineEdit   *m_pathLine;
+        QLinkedList<QListWidgetItem*> m_okItems;
         QListWidget *m_fsWidget;
+        QProgressBar    *m_loader;
         QPushButton *m_hideOkButton;
         QPushButton *m_processButton;
         QPushButton *m_searchButton;
@@ -36,12 +39,11 @@ class MainWidget : public QWidget
 
     public:
         MainWidget(QSettings &settings, QWidget *parent = 0);
-        void        dump(const DirStruct *current, int depth = -1, bool last = false);
 
     public slots:
         void        setPath(void);
         void        startCrawling(void);
-        DirStruct   *crawl(const QFileInfo &info, bool isRoot = false);
+        void        crawl(const QFileInfo &info, quint16 depth = 0, bool last = false);
         void        hideOk(void);
         void        showOk(void);
         void        process(void);
